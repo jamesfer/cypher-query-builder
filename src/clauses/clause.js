629ceb6aee04ceb2d3e9d9ef1066cc0b66713f55
James Ferguson <jamesferguson497@gmail.com>
2017-06-19 17:18:34 +1000

    Start implementing query parameters.

--- src/clauses/clause.js
+++ src/clauses/clause.js
@@ -1,10 +1,32 @@
 class Clause {
   constructor() {
+    this.params = {};
+  }
+
+  addParam(name, value) {
+    while (this.param[name] !== undefined) {
+      name += '1';
+    }
+
+    this.param[name] = value;
+  }
+
+  build() {
 
   }
 
-  toString() {
+  toQuery() {
     return '';
+  }
+
+  toString() {
+    let query = this.toQuery();
+    query.replace(/\$(\w+)/g, (fullMatch, paramName) => {
+      if (this.param[paramName] !== undefined) {
+        return this.param[paramName];
+      }
+      return fullMatch;
+    });
   }
 }
 module.exports = Clause;
