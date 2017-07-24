4aa94db4f5f68d1d43748f4b032b7759fad368b3
James Ferguson <jamesferguson497@gmail.com>
2017-07-24 21:21:41 +1000

    Change indentation to spaces.

--- src/connection.js
+++ src/connection.js
@@ -6,58 +6,58 @@ const Transformer = require('./transformer');
 let connections = [];
 
 // Closes all open connections
-nodeCleanup(function() {
-	connections.forEach(con => {
-		con.close();
-	});
-	connections = [];
+nodeCleanup(function () {
+  connections.forEach(con => {
+    con.close();
+  });
+  connections = [];
 });
 
 class Connection {
-	constructor(url, credentials) {
-		this.url = url;
-		this.auth = neo4j.auth.basic(credentials.username, credentials.password);
-		this.driver = neo4j.driver(this.url, this.auth);
-		this.open = true;
-		this.transformer = new Transformer();
-		connections.push(this);
-	}
+  constructor(url, credentials) {
+    this.url = url;
+    this.auth = neo4j.auth.basic(credentials.username, credentials.password);
+    this.driver = neo4j.driver(this.url, this.auth);
+    this.open = true;
+    this.transformer = new Transformer();
+    connections.push(this);
+  }
 
-	/**
-	 * Closes the connect if it is open.
-	 */
-	close() {
-		if (this.open) {
-			this.driver.close();
-			this.open = false;
-		}
-	}
+  /**
+   * Closes the connect if it is open.
+   */
+  close() {
+    if (this.open) {
+      this.driver.close();
+      this.open = false;
+    }
+  }
 
-	/**
-	 * Opens and returns a session.
-	 */
-	session() {
-		if (this.open) {
-			return this.driver.session();
-		}
-		return null;
-	}
+  /**
+   * Opens and returns a session.
+   */
+  session() {
+    if (this.open) {
+      return this.driver.session();
+    }
+    return null;
+  }
 
-	/**
-	 * Returns a new query that uses this connection.
-	 * @return {Query}
-	 */
-	query() {
-		return new Query(this);
-	}
+  /**
+   * Returns a new query that uses this connection.
+   * @return {Query}
+   */
+  query() {
+    return new Query(this);
+  }
 
-	/**
-	 * Runs a query in a session using this connection.
-	 * @param {Query} query
-	 * @return {Promise<Array>}
-	 */
-	run(query) {
-		if (!this.open) {
+  /**
+   * Runs a query in a session using this connection.
+   * @param {Query} query
+   * @return {Promise<Array>}
+   */
+  run(query) {
+    if (!this.open) {
       throw Error('Cannot run query; connection is not open.');
     }
 
@@ -66,7 +66,7 @@ class Connection {
     }
 
     let queryObj = query.buildQueryObject();
-		let session = this.session();
+    let session = this.session();
     return session.run(queryObj.query, queryObj.params)
       .then(result => {
         session.close();
@@ -76,22 +76,22 @@ class Connection {
         session.close();
         return Promise.reject(error);
       });
-	}
+  }
 }
 
 
 /** Query shortcut methods. */
 
 [
-	'matchNode',
-	'match',
-	'createNode',
-	'create',
-	'ret',
+  'matchNode',
+  'match',
+  'createNode',
+  'create',
+  'ret',
 ].forEach(name => {
-	Connection.prototype[name] = function () {
-		return Query.prototype[name].apply(this.query(), arguments);
-	}
+  Connection.prototype[name] = function () {
+    return Query.prototype[name].apply(this.query(), arguments);
+  };
 });
 
 
