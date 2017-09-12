fddf5996603438f53ea94d899b4462f264adc892
James Ferguson <jamesferguson497@gmail.com>
2017-09-12 14:59:58 +1000

    Create uniqueString utility function.

--- src/utils.js
+++ src/utils.js
@@ -15,6 +15,45 @@ module.exports.construct = function (cls, cb = null) {
 
 
 /**
+ * Converts a string to camel case and ensures it is unique in the provided
+ * list.
+ * @param {string} str
+ * @param {Array<string>} existing
+ * @return {string}
+ */
+module.exports.uniqueString = function(str, existing) {
+  str = _.camelCase(str);
+
+  // Check if the string already has a number extension
+  let number = null;
+  let matches = str.match(/[0-9]+$/);
+  if (matches) {
+    number = +matches[0];
+    str = str.substr(0, str.length - matches[0].length);
+  }
+
+  // Compute all taken suffixes that are similar to the given string
+  let regex = new RegExp('^' + str + '([0-9]*)$');
+  let takenSuffixes = _.reduce(existing, (suffixes, existingString) => {
+    let matches = existingString.match(regex);
+    if (matches) {
+      return [...suffixes, matches[1] ? +matches[1] : 1];
+    }
+    return suffixes;
+  }, []);
+
+  // If there was no suffix on the given string or it was already taken,
+  // compute the new suffix.
+  if (!number || takenSuffixes.indexOf(number) !== -1) {
+    number = (_.max(takenSuffixes) || 0) + 1;
+  }
+
+  // Append the suffix if it is not 1
+  return str + (number === 1 ? '' : number);
+};
+
+
+/**
  * Converts labels into a string that can be put into a pattern.
  *
  * @param {string|array<string>} labels
