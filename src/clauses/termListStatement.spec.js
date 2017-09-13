3366160ff96a6885115861329530c99e14a85c1d
James Ferguson <jamesferguson497@gmail.com>
2017-09-13 19:28:40 +1000

    Rename TermListClause to TermListStatement.

--- src/clauses/termListStatement.spec.js
+++ src/clauses/termListStatement.spec.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect;
-const TermListClause = require('./termListClause.js');
-const termListClauseTests = require('./termListClause.tests');
+const TermListClause = require('./termListStatement.js');
+const termListClauseTests = require('./termListStatement.tests');
 const { construct } = require('../utils');
 
-describe('TermListClause', function() {
+describe('TermListStatement', function() {
   describe('#build', function() {
     termListClauseTests(construct(TermListClause, s => s.buildQueryObject()), '');
   });
