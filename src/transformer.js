8bff3e1f1872928276f540c49983d0b62f5022dd
James Ferguson <jamesferguson497@gmail.com>
2017-09-14 14:03:01 +1000

    Change indentation to spaces.

--- src/transformer.js
+++ src/transformer.js
@@ -2,39 +2,39 @@ const _ = require('lodash');
 const neo4j = require('neo4j-driver').v1;
 
 class Transformer {
-	transformResult(result) {
-		return _.map(result.records, rec => this.transformRecord(rec));
-	}
+  transformResult(result) {
+    return _.map(result.records, rec => this.transformRecord(rec));
+  }
 
-	transformRecord(record) {
-		return _.mapValues(record.toObject(), node => {
-			return this.isNode(node) ? this.transformNode(node) : node;
-		});
-	}
+  transformRecord(record) {
+    return _.mapValues(record.toObject(), node => {
+      return this.isNode(node) ? this.transformNode(node) : node;
+    });
+  }
 
-	transformNode(node) {
-		return {
-			identity: neo4j.integer.toString(node.identity),
-			labels: node.labels,
-			properties: this.convertNumbers(node.properties),
-		};
-	}
+  transformNode(node) {
+    return {
+      identity: neo4j.integer.toString(node.identity),
+      labels: node.labels,
+      properties: this.convertNumbers(node.properties),
+    };
+  }
 
-	isNode(node) {
-		return node.identity && node.labels && node.properties;
-	}
+  isNode(node) {
+    return node.identity && node.labels && node.properties;
+  }
 
-	convertNumbers(object, recursive = true) {
-		return _.mapValues(object, value => {
-			if (!neo4j.isInt(value)) {
-				return value;
-			}
-			if (neo4j.integer.inSafeRange(value)) {
-				return neo4j.integer.toNumber(value);
-			}
-			return neo4j.integer.toString(value);
-		});
-	}
+  convertNumbers(object, recursive = true) {
+    return _.mapValues(object, value => {
+      if (!neo4j.isInt(value)) {
+        return value;
+      }
+      if (neo4j.integer.inSafeRange(value)) {
+        return neo4j.integer.toNumber(value);
+      }
+      return neo4j.integer.toString(value);
+    });
+  }
 }
 
 module.exports = Transformer;
