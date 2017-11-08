ad5ab60ef8197c9b57b4ea2ec368fcec04028906
James Ferguson <jamesferguson497@gmail.com>
2017-11-08 18:13:00 +1100

    Write proper tests for match clause.

--- src/clauses/match.spec.ts
+++ src/clauses/match.spec.ts
@@ -1,9 +1,25 @@
-import { construct } from '../utils';
 import { Match } from './match';
-import { matchTests } from './match.tests';
+import { NodePattern } from './node-pattern';
+import { expect } from 'chai';
 
 describe('Match', function() {
   describe('#build', function() {
-    matchTests(construct(Match, s => s.buildQueryObject()));
+    it('should start with MATCH', function() {
+      const create = new Match(new NodePattern('node'));
+      expect(create.build()).to.equal('MATCH (node)');
+    });
+
+    it('should start with OPTIONAL MATCH if optional is true', function() {
+      const create = new Match(new NodePattern('node'), { optional: true });
+      expect(create.build()).to.equal('OPTIONAL MATCH (node)');
+    });
+
+    it('should use expanded conditions', function() {
+      const create = new Match(new NodePattern('node', [], {
+        firstName: 'test',
+        lastName: 'test',
+      }));
+      expect(create.build()).to.equal('MATCH (node { firstName: $firstName, lastName: $lastName })');
+    });
   });
 });
