15375ea65031c83d4bd143ed3f85a034e7747521
James Ferguson <jamesferguson497@gmail.com>
2017-11-08 19:10:57 +1100

    Write proper tests for pattern statement.

--- src/clauses/patternStatement.spec.ts
+++ src/clauses/patternStatement.spec.ts
@@ -1,9 +1,40 @@
-import { patternStatementTests } from './patternStatement.tests';
-import { construct } from '../utils';
 import { PatternStatement } from './patternStatement';
+import { NodePattern } from './node-pattern';
+import { expect } from 'chai';
 
 describe('PatternStatement', function() {
   describe('#build', function() {
-    patternStatementTests(construct(PatternStatement, s => s.buildQueryObject()), '');
+    it('should accept a single pattern', function() {
+      let pattern = new PatternStatement(new NodePattern('a'));
+      expect(pattern.build()).to.equal('(a)');
+      expect(pattern.getParams()).to.be.empty;
+    });
+
+    it('should combine pattern sections with no delimiter', function() {
+      let pattern = new PatternStatement([
+        new NodePattern('a'),
+        new NodePattern('b'),
+        new NodePattern('c'),
+      ]);
+      expect(pattern.build()).to.equal('(a)(b)(c)');
+      expect(pattern.getParams()).to.be.empty;
+    });
+
+    it('should combine multiple patterns with a comma', function() {
+      let pattern = new PatternStatement([
+        [
+          new NodePattern('a'),
+          new NodePattern('b'),
+          new NodePattern('c'),
+        ],
+        [
+          new NodePattern('d'),
+          new NodePattern('e'),
+          new NodePattern('f'),
+        ]
+      ]);
+      expect(pattern.build()).to.equal('(a)(b)(c), (d)(e)(f)');
+      expect(pattern.getParams()).to.be.empty;
+    });
   });
 });
