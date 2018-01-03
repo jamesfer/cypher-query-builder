7081a8e3ac3a4bea0f0cfebc374ca1e54f8fbb1e
James Ferguson <jamesferguson497@gmail.com>
2018-01-03 16:04:35 +1100

    Fix order by clause.

--- src/clauses/order-by.spec.ts
+++ src/clauses/order-by.spec.ts
@@ -10,8 +10,19 @@ describe('OrderBy', function() {
   it('should support a direction arg', function() {
     let query = new OrderBy('node.prop', 'DESC');
     expect(query.build()).to.equal('ORDER BY node.prop DESC');
+    query = new OrderBy('node.prop', 'DESCENDING');
+    expect(query.build()).to.equal('ORDER BY node.prop DESC');
     query = new OrderBy('node.prop', 'ASC');
-    expect(query.build()).to.equal('ORDER BY node.prop ASC');
+    expect(query.build()).to.equal('ORDER BY node.prop');
+    query = new OrderBy('node.prop', 'ASCENDING');
+    expect(query.build()).to.equal('ORDER BY node.prop');
+  });
+
+  it('should support a boolean direction arg', function() {
+    let query = new OrderBy('node.prop', false);
+    expect(query.build()).to.equal('ORDER BY node.prop');
+    query = new OrderBy('node.prop', true);
+    expect(query.build()).to.equal('ORDER BY node.prop DESC');
   });
 
   it('should support multiple order columns', function() {
@@ -27,8 +38,9 @@ describe('OrderBy', function() {
   it('should support multiple order columns with directions', function() {
     let query = new OrderBy({
       'node.prop1': 'DESC',
-      'node.prop2': 'ASC'
+      'node.prop2': 'ASC',
+      'node.prop3': true
     });
-    expect(query.build()).to.equal('ORDER BY node.prop1 DESC, node.prop2 ASC');
+    expect(query.build()).to.equal('ORDER BY node.prop1 DESC, node.prop2, node.prop3 DESC');
   });
 });
