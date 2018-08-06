d0588aaf4aae6b19ff54e6d24a236c03633b774c
James Ferguson <jamesferguson497@gmail.com>
2018-08-07 09:29:25 +1000

    fix(Clause): match whole variable when inlining using interpolate
    Previously when inlining variables, a simple string replacement was used which meant that shorter
    variables could be inlined into longer ones. Eg. a variable "$param" could be inlined into the
    string "$paramName" to produce 'value'Name, which is obviously incorrect. The string replacement
    operation now attempts to only inline a param if it matches the entire variable.

--- src/clause.spec.ts
+++ src/clause.spec.ts
@@ -1,2 +1,28 @@
+import { Clause } from './clause';
+import { Where } from './clauses';
+import { expect } from 'chai';
+import { ParameterBag } from './parameter-bag';
+
 describe('Clause', () => {
+  describe('interpolate', () => {
+    it('should correctly inline parameters that share a prefix', () => {
+      class SpecialClause extends Clause {
+        constructor(public query: string) {
+          super();
+        }
+
+        build() {
+          return this.query;
+        }
+      }
+
+      const bag = new ParameterBag();
+      bag.addParam('abc', 'param');
+      bag.addParam('def', 'paramLong');
+
+      const clause = new SpecialClause('param = $paramLong');
+      clause.useParameterBag(bag);
+      expect(clause.interpolate()).to.equal(`param = 'def'`);
+    });
+  });
 });
