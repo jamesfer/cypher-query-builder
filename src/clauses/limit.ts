0f4a3d2e68a440ca78ebc70d33b7182e04122bc8
James Ferguson <jamesferguson497@gmail.com>
2018-08-08 20:28:37 +1000

    fix(Limit): use a parameter for limit number
    BREAKING CHANGE: A string expression as the limit number is no longer accepted. The argument must be
    a number.

--- src/clauses/limit.ts
+++ src/clauses/limit.ts
@@ -1,11 +1,15 @@
 import { Clause } from '../clause';
+import { Parameter } from '../parameter-bag';
 
 export class Limit extends Clause {
+  protected amountParam: Parameter;
+
   constructor(public amount: number | string) {
     super();
+    this.amountParam = this.addParam(amount, 'limitCount');
   }
 
   build() {
-    return `LIMIT ${this.amount}`;
+    return `LIMIT ${this.amountParam}`;
   }
 }
