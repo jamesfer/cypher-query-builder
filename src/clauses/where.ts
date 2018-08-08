f664ebe50825a9a6ad6e991f1b7746d1d6fc64d6
James Ferguson <jamesferguson497@gmail.com>
2018-08-08 20:47:14 +1000

    fix(Where): remove side effects from build
    Previously, calling `build()` on a where clause would add more parameters each time. This side
    effect has been moved so parameters are created in the constructor.
    
    Closes #42

--- src/clauses/where.ts
+++ src/clauses/where.ts
@@ -1,28 +1,15 @@
 import { Clause } from '../clause';
 import { AnyConditions, stringCons } from './where-utils';
 
-/**
- * where({
- *    n: {
- *      name: 'james',
- *      active: eq(1),
- *      age: gt(20),
- *    }
- * });
- * where({
- *   'n.name': 'james'
- * });
- * where([
- *   { 'n.name': 'james' },
- *   { 'n.age': gt(20) },
- * ]);
- */
 export class Where extends Clause {
+  protected query: string;
+
   constructor(public conditions: AnyConditions) {
     super();
+    this.query = stringCons(this.parameterBag, this.conditions);
   }
 
   build() {
-    return 'WHERE ' + stringCons(this.parameterBag, this.conditions);
+    return 'WHERE ' + this.query;
   }
 }
