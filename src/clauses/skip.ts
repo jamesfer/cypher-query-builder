d6fc16102930b6f0b616c79add59ce87b6187e71
James Ferguson <jamesferguson497@gmail.com>
2018-08-08 20:29:49 +1000

    fix(Skip): use a parameter in skip number
    BREAKING CHANGE: A string expression as the skip number is no longer accepted. The argument must be
    a number.

--- src/clauses/skip.ts
+++ src/clauses/skip.ts
@@ -1,11 +1,14 @@
 import { Clause } from '../clause';
 
 export class Skip extends Clause {
+  protected amountParam;
+
   constructor(public amount: number | string) {
     super();
+    this.amountParam = this.addParam(amount, 'skipCount');
   }
 
   build() {
-    return `SKIP ${this.amount}`;
+    return `SKIP ${this.amountParam}`;
   }
 }
