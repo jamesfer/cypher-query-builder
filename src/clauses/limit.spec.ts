7f6360c26687d23263c590b9e7ad91b754c46378
James Ferguson <jamesferguson497@gmail.com>
2018-08-08 20:29:49 +1000

    fix(Skip): use a parameter in skip number
    BREAKING CHANGE: A string expression as the skip number is no longer accepted. The argument must be
    a number.

--- src/clauses/limit.spec.ts
+++ src/clauses/limit.spec.ts
@@ -8,11 +8,5 @@ describe('Limit', () => {
       expect(query.build()).to.equal('LIMIT $limitCount');
       expect(query.getParams()).to.eql({ limitCount: 10 });
     });
-
-    // TODO re-add in 4.0
-    it.skip('should accept a string param', () => {
-      const query = new Limit('toInteger(3 * 4)');
-      expect(query.build()).to.equal('LIMIT toInteger(3 * 4)');
-    });
   });
 });
