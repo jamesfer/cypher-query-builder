025c87341feb83ebb2b7200b75f8aa957f503d01
James Ferguson <jamesferguson497@gmail.com>
2018-08-08 20:28:37 +1000

    fix(Limit): use a parameter for limit number
    BREAKING CHANGE: A string expression as the limit number is no longer accepted. The argument must be
    a number.

--- src/clauses/limit.spec.ts
+++ src/clauses/limit.spec.ts
@@ -5,10 +5,12 @@ describe('Limit', () => {
   describe('#build', () => {
     it('should add a produce a limit clause', () => {
       const query = new Limit(10);
-      expect(query.build()).to.equal('LIMIT 10');
+      expect(query.build()).to.equal('LIMIT $limitCount');
+      expect(query.getParams()).to.eql({ limitCount: 10 });
     });
 
-    it('should accept a string param', () => {
+    // TODO re-add in 4.0
+    it.skip('should accept a string param', () => {
       const query = new Limit('toInteger(3 * 4)');
       expect(query.build()).to.equal('LIMIT toInteger(3 * 4)');
     });
