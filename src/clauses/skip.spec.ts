1b94118dff37fa6c835a29333b965a6a04087f28
James Ferguson <jamesferguson497@gmail.com>
2018-10-31 18:50:49 +1100

    fix: update code style and test errors

--- src/clauses/skip.spec.ts
+++ src/clauses/skip.spec.ts
@@ -8,11 +8,5 @@ describe('Skip', () => {
       expect(query.build()).to.equal('SKIP $skipCount');
       expect(query.getParams()).to.eql({ skipCount: 10 });
     });
-
-    // TODO re-enable in 4.0
-    it.skip('should accept a string param', () => {
-      const query = new Skip('toInteger(3 * 4)');
-      expect(query.build()).to.equal('SKIP toInteger(3 * 4)');
-    });
   });
 });
