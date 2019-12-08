6dde4944f63aa8330061a46a686f8fd2940bb5b9
James Ferguson <jamesferguson497@gmail.com>
2019-12-08 20:16:02 +1100

    fix: export builder, clause and clause collection
    Export some of the internal classes to make it easier to integrate and extend.
    
    fix #116

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 /* istanbul ignore file */
+export * from './builder';
 export * from './connection';
+export * from './clause';
+export * from './clause-collection';
 export * from './clauses';
 export * from './query';
 export * from './transformer';
