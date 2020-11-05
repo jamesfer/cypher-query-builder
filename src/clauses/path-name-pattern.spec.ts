e856d25ebc93ec651cbb8f8a98bfbc44b1cc8676
Bastian <bastian@maramoja.co.ke>
2020-11-05 18:55:41 +0300

    Add test assertion for the built string of PathNamePattern.

--- src/clauses/path-name-pattern.spec.ts
+++ src/clauses/path-name-pattern.spec.ts
@@ -4,8 +4,9 @@ import { expect } from 'chai';
 describe('PathNamePattern', () => {
 
   it('should accept just a name', () => {
-    const pattern = new PathNamePattern('label');
+    const pattern = new PathNamePattern('p');
     expect(pattern.getLabelsString()).to.equal('');
-    expect(pattern.getNameString()).to.equal('label');
+    expect(pattern.getNameString()).to.equal('p');
+    expect(pattern.build()).to.equal('p=');
   });
 });
